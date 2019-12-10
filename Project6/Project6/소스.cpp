a2b6a619032d86f4bb9e115baf695e67acd912e8
수현 이 <7553321@naver.com>
2019-12-10 22:33:32 +0900

    히히

---
+++ Project6/Project6/소스.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	double degree[] = { -5.34,3.67,19.76,28.76,35.63 };
+	int size = sizeof(degree) / sizeof(double);
+
+	for (int i = 0;i < size;i++) {
+		printf("%.2lf ", degree[i]);
+	}
+}
